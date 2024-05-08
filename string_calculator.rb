e44e3fc9bfee79bb1d408640a9281a7ef962fe73
keerthi524 <keerthi.mosam@gmail.com>
2024-05-08 12:41:52 +0530

    When an negative integer is sent in the string it raises error

--- string_calculator.rb
+++ string_calculator.rb
@@ -7,5 +7,13 @@ class StringCalculator
 
   def add
     return 0 if string.empty?
+
+    raise_error_if_negative
+  end
+
+  def raise_error_if_negative
+    # checks if any negative numbers are present in the string
+    negatives = string.split(',').select { |i| i.to_i < 0 }.join(',')
+    raise "Negative Numbers are #{negatives}" unless negatives.empty?
   end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -5,10 +5,19 @@ describe StringCalculator do
   let(:calculator) { described_class.new(string) }
 
   describe '#add_string' do
-    context 'When an empty number is sent' do
+    context 'When an empty number is sent in the string' do
       let(:string) { '' }
+
       it 'returns 0' do
         expect(calculator.add).to eq(0)
+      end
+    end
+
+    context 'When an negative integer is sent in the string' do
+      let(:string) { '-4,-9' }
+
+      it 'raises an exception with message' do
+        expect { calculator.add }.to raise_error('Negative Numbers are -4,-9')
       end
     end
   end
